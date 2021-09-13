69ff40894f6e8fe204821d7472133c588286d1af
zabir122 <58477430+zabir122@users.noreply.github.com>
2021-09-14 01:12:40 +0600

    Add files via upload

--- mendeley-catalog.py.py
+++ mendeley-catalog.py.py
@@ -2,30 +2,18 @@ from mendeley import Mendeley
 import yaml
 import os
 import csv
-# Get the DOI to look up
-import argparse
-# parser = argparse.ArgumentParser()
-# parser.add_argument("doi", help="Searches the Mendeley catalogue for this DOI")
-# args = parser.parse_args()
 
-# config_file = 'config.yml'
 
 config = {}
 
-# if os.path.isfile(config_file):
-#     with open('config.yml') as f:
-#         config = yaml.load(f)
-# else:
-# config['clientId'] = os.environ.get('MENDELEY_CLIENT_ID')
-config['clientId'] = 10787
-# config['clientSecret'] = os.environ.get('MENDELEY_CLIENT_SECRET')
+
 config['clientSecret'] = 'idravljCIiczdX8y'
 
 mendeley = Mendeley(config['clientId'], config['clientSecret'])
-# mendeley = Mendeley(10467, 'bdrMTaTJXLD1EHf7')
+
 session = mendeley.start_client_credentials_flow().authenticate()
 
-# doi = args.doi
+
 
 
 with open('bangladesh 2016-2020 16501-17000.csv', encoding="utf-8-sig") as f:
@@ -35,29 +23,24 @@ with open('bangladesh 2016-2020 16501-17000.csv', encoding="utf-8-sig") as f:
 
     for row in reader:
         doi.append(row["DOI"])
-
+        doi="".join(doi).split()
         print(doi)
 
         j = 0
         mysrting = " "
         i = 0
 
-        for i in range(len(doi)):
-            if doi is mysrting:
-                pass
-
-            else:
-                #try:
+        for x in doi:
+         
 
                     v = str(j)+".txt"
                     with open(v, mode='w') as file:
                         doc = session.catalog.by_identifier(
-                            doi=doi[i], view='stats')
+                            doi=x, view='stats')
                         print(session.catalog)
-                        # if doc  is None:
-                        # pass
+                 
 
-                        # else:
+                  =
 
                         print('"%s" has %s readers.' %
                               (doc.title,  doc.reader_count))
@@ -73,6 +56,6 @@ with open('bangladesh 2016-2020 16501-17000.csv', encoding="utf-8-sig") as f:
                         file.write(str(doc.reader_count_by_subdiscipline))
                         file.write('\n')
                         j = j+1
-                #except:
+            
 
-            i = i+1
+            
